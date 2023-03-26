4a678be9c9ad18d5fedc48f81b04665e7a0f5499
jesselessa <jessica.elessa@gmail.com>
2023-03-26 06:09:52 +0200

    optimized code by creating populateCountryInfo function

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,32 @@
 //! The ready function executes the code after the DOM is fully loaded
 
 $().ready(() => {
+  //* Function populateCountryInfo
+  function populateCountryInfo(data) {
+    data.forEach((country) => {
+      $("#countriesList").append(`
+        <li class="country">
+          <div class="countryInfo">
+            <p><span>Country :</span> ${country.name.common}</p>
+            <p><span>Capital(s) :</span> ${country.capital}</p>
+            <p class="currency"><span>Continent :</span> ${country.region}</p>
+              <p><span>Currency(ies) :</span> ${
+                country.currencies && Object.keys(country.currencies) // If no short-circuit operator, warning message
+              }</p>
+          </div>
+        </li>`);
+    });
+  }
+
   //* Display list of all countries after loading the page
   $.ajax({
     url: "https://restcountries.com/v3.1/all",
     dataType: "json",
-    success: (data) => {
-      data.forEach((country) => {
-        $("#countriesList").append(`
-          <li class="country">
-            <div class="countryInfo">
-              <p><span>Country :</span> ${country.name.common}</p>
-              <p><span>Capital(s) :</span> ${country.capital}</p>
-              <p class="currency"><span>Continent :</span> ${country.region}</p>
-                <p><span>Currency(ies) :</span> ${
-                  country.currencies && Object.keys(country.currencies)
-                }</p>
-            </div>
-          </li>`);
-      });
-    },
+    success: populateCountryInfo,
     error: (error) => {
       console.log(error);
       alert("A problem occured. Come back later.");
     },
-  });
-
-  //* Collect API data when submitting form
-  $("form").submit(function (e) {
-    e.preventDefault();
-
-    getAllCountries();
   });
 
   //* Function getAllCountries with a dynamic URL
@@ -49,26 +45,20 @@ $().ready(() => {
       url: url,
       data: "application/x-www-form-urlencoded",
       dataType: "json",
-      success: (data) => {
-        $("#countriesList").empty();
-        data.forEach((country) => {
-          $("#countriesList").append(`
-          <li class="country">
-            <div class="countryInfo">
-              <p><span>Country :</span> ${country.name.common}</p>
-              <p><span>Capital(s) :</span> ${country.capital}</p>
-              <p class="currency"><span>Continent :</span> ${country.region}</p>
-                <p><span>Currency(ies) :</span> ${
-                  country.currencies && Object.keys(country.currencies)
-                }</p>                          
-            </div>
-          </li>`);
-        });
-      },
+      success: populateCountryInfo,
       error: (error) => {
         console.log(error);
-        alert("A problem occured. Come back later.");
+        alert(
+          "A problem occured. First, make sure you selected the right category or entered a valid name. If so, come back later."
+        );
       },
     });
   }
+
+  //* Collect API data when submitting form
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    getAllCountries();
+  });
 });
