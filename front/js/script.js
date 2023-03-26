4689e56f7dfbd1988eb25156671b28a7211423bd
jesselessa <jessica.elessa@gmail.com>
2023-03-26 12:10:16 +0200

    changed name of displayAllCountries function and fixed loader display

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,71 @@
 //! The ready function executes the code after the DOM is fully loaded
-
 $().ready(() => {
+  //* Display list of all countries
+  function showAllCountries() {
+    $.ajax({
+      url: "https://restcountries.com/v3.1/all",
+      dataType: "json",
+      success: populateCountryInfo,
+      error: (error) => {
+        console.log(error);
+        alert("A problem occured. Come back later.");
+      },
+    });
+  }
+  showAllCountries();
+
   //* Populate countries info
   function populateCountryInfo(data) {
     data.forEach((country) => {
       $("#countriesList").append(`
-        <li class="country">
-          <div class="countryInfo">
-            <p><span>Country :</span> ${country.name.common}</p>
-            <p><span>Capital(s) :</span> ${country.capital}</p>
-            <p class="currency"><span>Continent :</span> ${country.region}</p>
-              <p><span>Currency(ies) :</span> ${
-                country.currencies && Object.keys(country.currencies) // If no short-circuit operator, warning message
-              }</p>
-          </div>
-        </li>`);
+      <li class="country">
+        <div class="countryInfo">
+          <p><span>Country :</span> ${country.name.common}</p>
+          <p><span>Capital(s) :</span> ${country.capital}</p>
+          <p class="currency"><span>Continent :</span> ${country.region}</p>
+            <p><span>Currency(ies) :</span> ${
+              country.currencies && Object.keys(country.currencies) //! If no short-circuit operator, warning message
+            }</p>
+        </div>
+      </li>`);
     });
   }
+
+  //* Display data when submitting form
+  function showData() {
+    $("form").submit((e) => {
+      e.preventDefault();
+
+      $(".country").remove();
+
+      showSpinner(); // Display spinner
+
+      setTimeout(hideSpinner, 1000); // After 1 second delay, hide spinner
+
+      setTimeout(getAllCountries, 1000); // After 1 second delay, display data
+    });
+  }
+  showData();
+
+  //* Show or hide loader
+  function showSpinner() {
+    $("#loader").css("display", "block");
+  }
+
+  function hideSpinner() {
+    $("#loader").css("display", "none");
+  }
+
+  //* Reset form
+  function reset() {
+    $("#btnReset").click(() => {
+      $("form")[0].reset();
+      $("#countriesList").empty(); // To remove data previously displayed
+
+      showAllCountries();
+    });
+  }
+  reset();
 
   //* Get countries info by name, capital or continent
   function getAllCountries() {
@@ -43,41 +92,4 @@ $().ready(() => {
       },
     });
   }
-
-  //* Display list of all countries
-  function displayAllCountries() {
-    $.ajax({
-      url: "https://restcountries.com/v3.1/all",
-      dataType: "json",
-      success: populateCountryInfo,
-      error: (error) => {
-        console.log(error);
-        alert("A problem occured. Come back later.");
-      },
-    });
-  }
-  displayAllCountries();
-
-  //* Display data when submitting form
-  function showData() {
-    $("form").submit((e) => {
-      e.preventDefault();
-
-      $(".country").remove();
-
-      getAllCountries();
-    });
-  }
-  showData();
-
-  //* Reset form
-  function reset() {
-    $("#btnReset").click(() => {
-      $("form")[0].reset();
-      $("#countriesList").empty();
-
-      displayAllCountries();
-    });
-  }
-  reset();
 });
