759b4567ae2a433748ee112aaad3abb659b43c33
jesselessa <jessica.elessa@gmail.com>
2023-03-26 04:41:56 +0200

    improved function by adding search by name, capital or continent

--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,68 @@
-// The ready function executes the code only when the DOM is fully loaded
-$(document).ready(() => {
-  // * getAllCountries Function
+//! The ready function executes the code after the DOM is fully loaded
+
+$().ready(() => {
+  //* Display list of all countries after loading the page
+  $.ajax({
+    url: "https://restcountries.com/v3.1/all",
+    dataType: "json",
+    success: (data) => {
+      data.forEach((country) => {
+        $("#countriesList").append(`
+          <li class="country">
+            <div class="countryInfo">
+              <p><span>Country :</span> ${country.name.common}</p>
+              <p><span>Capital(s) :</span> ${country.capital}</p>
+              <p class="currency"><span>Continent :</span> ${country.region}</p>
+                <p><span>Currency(ies) :</span> ${
+                  country.currencies && Object.keys(country.currencies)
+                }</p>
+            </div>
+          </li>`);
+      });
+    },
+    error: (error) => {
+      console.log(error);
+      alert("A problem occured. Come back later.");
+    },
+  });
+
+  //* Collect API data when submitting form
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    getAllCountries();
+  });
+
+  //* Function getAllCountries with a dynamic URL
   function getAllCountries() {
+    let url,
+      inputValue = $("#name").val(); // To retrieve value of input
+    if ($("#country-btn").is(":checked")) {
+      url = `https://restcountries.com/v3.1/name/${inputValue}`;
+    } else if ($("#capital-btn").is(":checked")) {
+      url = `https://restcountries.com/v3.1/capital/${inputValue}`;
+    } else if ($("#continent-btn").is(":checked")) {
+      url = `https://restcountries.com/v3.1/region/${inputValue}`;
+    }
+
     $.ajax({
-      url: "https://restcountries.com/v3.1/all",
-      type: "GET",
+      url: url,
+      data: "application/x-www-form-urlencoded",
       dataType: "json",
       success: (data) => {
-        console.log(data);
+        $("#countriesList").empty();
         data.forEach((country) => {
           $("#countriesList").append(`
-                  <li class="country">
-                    <div class="countryInfo">
-                      <p><span>Country :</span> ${country.name.common}</p>
-                      <p><span>Capital(s) :</span> ${country.capital}</p>
-                      <p class="currency"><span>Continent :</span> ${
-                        country.region
-                      }</p>
-                        <p><span>Currency(ies) :</span> ${
-                          country.currencies && Object.keys(country.currencies)
-                        }</p>                          
-                    </div>
-                  </li>`);
+          <li class="country">
+            <div class="countryInfo">
+              <p><span>Country :</span> ${country.name.common}</p>
+              <p><span>Capital(s) :</span> ${country.capital}</p>
+              <p class="currency"><span>Continent :</span> ${country.region}</p>
+                <p><span>Currency(ies) :</span> ${
+                  country.currencies && Object.keys(country.currencies)
+                }</p>                          
+            </div>
+          </li>`);
         });
       },
       error: (error) => {
@@ -30,6 +71,4 @@ $(document).ready(() => {
       },
     });
   }
-
-  getAllCountries();
 });
