f7845577976e1669a30e4d4332878bceeab3f82b
jesselessa <jessica.elessa@gmail.com>
2023-04-07 12:48:36 +0200

    created route to get all countries

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
-//------------- EXPRESS --------------//
-import express from "express";
+//--------------- EXPRESS ---------------//
+const express = require("express");
 const app = express();
+//-------------- JSON -------------------//
+const countriesData = require("./countriesData.json");
 
+//--------------- ROUTES ---------------//
+//* Get all countries
+app.get("/all", (_req, res) => {
+  res.json({
+    status: "success",
+    data: countriesData,
+  });
+});
+
+//* Handle errors
+app.get("*", (_req, res) => {
+  res.status(404).send("Error 404 - Not found");
+});
 //----------- START SERVER ----------//
 const port = 8000;
-app.listen(port, ()=> {console.log(`Server listening at http://localhost:${port}`)})
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
