97418fc2c53b90c228f53e0b0f4bc01f6020d1ec
jesselessa <jessica.elessa@gmail.com>
2023-04-08 00:03:33 +0200

    created routes to get countries by name, capital and continent

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,53 @@ const app = express();
 const countriesData = require("./countriesData.json");
 
 //--------------- ROUTES ---------------//
+
+//* Root page
+app.get("/", (_req, res) => {
+  res.status(200).send("This is Countries API");
+});
+
 //* Get all countries
 app.get("/all", (_req, res) => {
-  res.json({
-    status: "success",
-    data: countriesData,
+  res.status(200).json({
+    countriesData,
   });
+});
+
+//* Get a country by name
+app.get("/country/:country", (req, res) => {
+  const country = countriesData.find((countryName) => {
+    return (
+      countryName.name.common.toLowerCase().replace(" ", "") ===
+      req.params.country.toLowerCase().replace(" ", "")
+    );
+  });
+
+  res.status(200).json(country);
+});
+
+//* Get a country by capital
+app.get("/capital/:capital", (req, res) => {
+  const capital = countriesData.find((capitalName) => {
+    return (
+      capitalName.capital.toString().toLowerCase().replace(" ", "") ===
+      req.params.capital.toLowerCase().replace(" ", "")
+    );
+  });
+
+  res.status(200).json(capital);
+});
+
+//* Get a country by continent
+app.get("/continent/:continent", (req, res) => {
+  const countries = countriesData.filter((country) => {
+    return (
+      country.region.toLowerCase().replace(" ", "") ===
+      req.params.continent.toLowerCase().replace(" ", "")
+    );
+  });
+
+  res.status(200).json(countries);
 });
 
 //* Handle errors
